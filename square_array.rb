ab89b537ebdbc02f63d62d0f3f26063669094540
Kevin Chi <kevinchi118@gmail.com>
2018-06-12 22:04:47 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,6 @@
 def square_array(array)
-    new_array = []
     array.each do |element|
-        element_squared = element ** 2
-        new_array.push(element_squared)
-    end
-    return new_array
+        element = element ** 2
+    end 
+
 end
